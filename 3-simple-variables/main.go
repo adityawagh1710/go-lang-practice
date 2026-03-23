ab04b508179e7a752476fc8e57bdab04280bbc62
Aditya Wagh <ad.wagh@easternenterprise.com>
2026-03-23 20:26:29 +0530

    feat: practice examples for go lang slices

--- 3-simple-variables/main.go
+++ 3-simple-variables/main.go
@@ -1,6 +1,8 @@
 package main
 import ("fmt")
 
+var c, python1, java2 bool
+
 func main() {
   var student1 string = "Napster"
   var student2 = "Tester" 
@@ -9,4 +11,11 @@ func main() {
   fmt.Println(student1)
   fmt.Println(student2)
   fmt.Println(x)
+
+  var i, j int = 1, 2
+	k := 3
+	c, python, java := true, false, "no!"
+
+	fmt.Println(i, j, k, c, python, java)
+  fmt.Println(i, c, python1, java1)
 }
---
+++ 9-slices/slices.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func main() {
+	// Slice with length = 3, capacity = 5
+	nums := make([]int, 3, 5)
+
+	fmt.Println("Slice length capacity ", nums)
+	fmt.Println("len ", len(nums))
+	fmt.Println("cap ", cap(nums))
+
+	// Access and update array by index
+	nums2 := []int{10, 20, 30}
+	fmt.Println("Slice 0 index ", nums2[0])
+	nums2[1] = 50
+	fmt.Println("Slice 1 index ", nums2)
+
+	// Slice append to attach multiple array elements
+	nums3 := make([]int, 0, 5)
+	nums3 = append(nums3, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+	fmt.Println("Append ", nums3)
+
+	// Append array elemnts for slice
+	nums4 := make([]int, 0, 5)
+	nums4 = append(nums4, len(nums4))
+	nums4 = append(nums4, 7)
+	fmt.Println("Append slice", nums4)
+
+	// Copy slice from one to new
+	src := []int{1, 2, 3}
+	dest := make([]int, len(src))
+	copy(dest, src)
+	fmt.Println("Copy ", src, dest)
+
+	// Slice operator
+	nums5 := []int{1, 2, 3, 4, 5, 6, 7}
+	fmt.Println("Slice 0-2", nums5[0:2])
+	fmt.Println("Slice all after 2", nums5[:2])
+	fmt.Println("Slice only first 2", nums5[2:])
+
+	// Slice package
+	nums6 := []int{1, 2, 3, 4, 5, 7}
+	nums7 := []int{1, 2, 3, 4, 5, 7}
+	fmt.Println("Slice equal", slices.Equal(nums6, nums7))
+}
