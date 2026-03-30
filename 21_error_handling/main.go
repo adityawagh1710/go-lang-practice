5a68dd5a2775c7dfa4f85bded8e81b11c783b9da
Aditya Wagh <ad.wagh@easternenterprise.com>
2026-03-30 12:19:37 +0530

    feat: go lang examples for error handling

---
+++ 21_error_handling/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	// ErrBadRequest is returned when there are problems with the request.
+	ErrBadRequest = errors.New("Bad Request")
+
+	// ErrPageMoved is returned when a 301/302 is returned.
+	ErrPageMoved = errors.New("Page Moved")
+)
+
+func main() {
+
+	err := errors.New("an error occurred")
+	fmt.Println(err)
+
+	if err := webCall(false); err != nil {
+		switch err {
+		case ErrBadRequest:
+			fmt.Println("Bad Request Occurred")
+			return
+
+		case ErrPageMoved:
+			fmt.Println("The Page moved")
+			return
+
+		default:
+			fmt.Println(err)
+			return
+		}
+	}
+
+	fmt.Println("Life is good")
+
+}
+
+// webCall performs a web operation.
+func webCall(b bool) error {
+	if b {
+		return ErrBadRequest
+	}
+
+	return ErrPageMoved
+}
