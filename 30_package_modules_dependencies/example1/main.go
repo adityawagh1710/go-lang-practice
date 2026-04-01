5799f8639df6f640957090bb798c0bf5b3cf766d
Aditya Wagh <ad.wagh@easternenterprise.com>
2026-04-01 20:23:31 +0530

    feat: go lang examples for package modules dependencies and idiomatic composition

--- 30_package_modules_dependencies/example1/main.go
+++ 30_package_modules_dependencies/example1/main.go
@@ -5,16 +5,19 @@ import (
 	"strings"
 
 	"github.com/pborman/uuid"
+	"github.com/fatih/color"
 )
 
 func main() {
 	// How module dependancies work
-	// 1. go mod init learn
-	// 2. go mod tidy
-	// 3. go mod tidy -v
-	// 4. 
+	// 1. go get ******
+	// 2. go mod init learn
+	// 3. go mod tidy
+	// 4. go mod tidy -v
+	// 5.
 
 	uuidWithHyphen := uuid.NewRandom()
 	uuid := strings.Replace(uuidWithHyphen.String(), "-", "", -1)
 	fmt.Println(uuid)
+	color.Cyan(uuid)
 }
--- go.mod
+++ go.mod
@@ -1,7 +1,16 @@
 module learn
 
-go 1.22.0
+go 1.25.0
 
 require github.com/pborman/uuid v1.2.1
 
-require github.com/google/uuid v1.6.0 // indirect
+require (
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.42.0 // indirect
+)
+
+require (
+	github.com/fatih/color v1.19.0
+	github.com/google/uuid v1.6.0 // indirect
+)
