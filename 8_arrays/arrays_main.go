83b9574d375567baa5186e1905603228df0085c2
Aditya Wagh <ad.wagh@easternenterprise.com>
2026-04-07 21:39:59 +0530

    feat: go lang examples for arrays

--- 8_arrays/arrays_main.go
+++ 8_arrays/arrays_main.go
@@ -24,4 +24,14 @@ func main() {
 
 	// Fixed size, that is predictable
 	// Memeory optimization
+
+	// declares two arrays (arr1 and arr2) with inferred lengths
+	var arr1 = [...]int{1, 2, 3}
+	arr2 := [...]int{4, 5, 6, 7, 8}
+	fmt.Println(arr1)
+	fmt.Println(arr2)
+
+	// initializes only the second and third elements of the array
+	arr3 := [5]int{1: 10, 2: 40}
+	fmt.Println(arr3)
 }
